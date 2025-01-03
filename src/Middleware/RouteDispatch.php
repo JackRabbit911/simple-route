8c07a12386d2a955259493c4131280cc13a5e4bf
alx <world.zay@gmail.com>
2025-01-03 15:25:43 +0300

    Updated RouteDispatch, Created Resolver

--- src/Middleware/RouteDispatch.php
+++ src/Middleware/RouteDispatch.php
@@ -2,78 +2,32 @@
 
 namespace Az\Route\Middleware;
 
-use Az\Route\NormalizeResponse;
+use Az\Route\Resolver;
 use Az\Route\Route;
-use Invoker\InvokerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Closure;
 
 class RouteDispatch implements MiddlewareInterface
 {
-    use NormalizeResponse;
+    private Resolver $resolver;
 
-    private InvokerInterface $container;
-
-    public function __construct(InvokerInterface $container)
+    public function __construct(Resolver $resolver)
     {
-        $this->container = $container;
+        $this->resolver = $resolver;
     }
-    public function process(
-        ServerRequestInterface $request,
-        RequestHandlerInterface $handler
-    ): ResponseInterface
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         if (!$route = $request->getAttribute(Route::class)) {
             return $handler->handle($request);
         }
 
         $routeHandler = $route->getHandler();
-        $params = array_merge(['request' => $request], $route->getParameters());
+        $instance = $this->resolver->resolve($routeHandler);
+        $response = $instance->handle($request);
 
-        if (!$routeHandler instanceof Closure) {
-            if (is_a($routeHandler[0] ?? $routeHandler, MiddlewareInterface::class, true)) {
-                $class = $routeHandler[0] ?? $routeHandler;
-                $middleware = $this->container->make($class);
-            } elseif (is_a($routeHandler[0] ?? $routeHandler, RequestHandlerInterface::class, true)) {
-                $routeHandler[1] = 'handle';
-            }
-        }
-
-        if (!isset($middleware)) {
-            $middleware = $this->wrapper($this->container, $routeHandler, $params);
-        }
-
-        return $middleware->process($request, $handler);
-    }
-
-    private function wrapper($container, $routeHandler, $params): MiddlewareInterface
-    {
-        return new class ($container, $routeHandler, $params) implements MiddlewareInterface
-        {
-            use NormalizeResponse;
-            
-            private InvokerInterface $container;
-            private $routeHandler;
-            private $params;
-
-            public function __construct(InvokerInterface $container, $routeHandler, $params)
-            {
-                $this->container = $container;
-                $this->routeHandler = $routeHandler;
-                $this->params = $params;
-            }
-
-            public function process(
-                ServerRequestInterface $request,
-                RequestHandlerInterface $handler
-            ): ResponseInterface
-            {
-                $response = $this->container->call($this->routeHandler, $this->params);
-                return $this->normalizeResponse($request, $response);
-            }
-        };
+        return $response;
     }
 }
---
+++ src/Resolver.php
@@ -0,0 +1,105 @@
+<?php declare(strict_types=1);
+
+namespace Az\Route;
+
+use Az\Route\Route;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Container\ContainerInterface;
+use Closure;
+
+class Resolver
+{
+    private ?ContainerInterface $container;
+
+    public function __construct(?ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function resolve($handler)
+    {
+        if (is_array($handler)) {
+            return $this->array($handler);
+        }
+
+        if ($handler instanceof RequestHandlerInterface) {
+            return $handler;
+        }
+
+        if ($handler instanceof Closure || function_exists($handler)) {
+            return $this->func($handler);
+        }
+    }
+
+    private function func($handler)
+    {
+        return new class ($handler, $this->container) implements RequestHandlerInterface
+        {
+            private $func;
+            private ContainerInterface $container;
+
+            public function __construct(Closure|string $handler, ContainerInterface $container)
+            {
+                $this->func = $handler;
+                $this->container = $container;
+            }
+
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                $route = $request->getAttribute(Route::class);
+                $params = array_merge(['request' => $request], $route->getParameters());
+
+                return (method_exists($this->container, 'call')) 
+                    ? $this->container->call($this->func, $params)
+                    : call_user_func_array($this->func, array_values($params));
+            }
+        };
+    }
+
+    private function array(array $handler)
+    {
+        $instance = $this->getInstance($handler[0]);
+
+        if ($instance instanceof RequestHandlerInterface) {
+            return $instance;
+        }
+
+        return new class ($instance, $this->container) implements RequestHandlerInterface
+        {
+            private $instance;
+            private $container;
+
+            public function __construct($instance, ?ContainerInterface $container)
+            {
+                $this->instance = $instance;
+                $this->container = $container;
+            }
+
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                $route = $request->getAttribute(Route::class);
+                $action = $route->getHandler()[1];
+                $params = $route->getParameters();
+
+                return (method_exists($this->container, 'call')) 
+                    ? $this->container->call([$this->instance, $action], $params)
+                    : call_user_func_array([$this->instance, $action], array_values($params));
+            }
+        };
+    }
+
+    private function getInstance($handler)
+    {
+        if (is_object($handler) || function_exists($handler)) {
+            return $handler;
+        }
+
+        if (method_exists($this->container, 'make')) {
+            return $this->container->make($handler);
+        }
+
+        return $this->container->get($handler);
+    }
+}
