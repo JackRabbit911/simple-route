7f43866f121e503fb45d54b34ca61f94d9e6a10d
alx <world.zay@gmail.com>
2025-01-09 17:08:42 +0300

    Created ArticleRepo class, updated SimpleHandler

--- demo/app/Controller/SimpleHandler.php
+++ demo/app/Controller/SimpleHandler.php
@@ -2,20 +2,53 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticlesRepo;
 use Az\Route\Route;
-use HttpSoft\Response\HtmlResponse;
 
 #[Route(methods: 'any')]
 class SimpleHandler
 {
-    public function __invoke()
+    private ArticlesRepo $repo;
+    private array $menu;
+
+    public function __construct(ArticlesRepo $repo)
     {
-        return new HtmlResponse('Articles list');
+        $this->repo = $repo;
+        $this->menu = require '../app/config/menu.php';
     }
 
-    // #[Route(methods: ['post', 'put'])]
+    public function __invoke()
+    {
+        $list = $this->repo->getList();
+
+        $data = [
+            'title' => 'Articles list',
+            'menu'  => $this->menu,
+            'cont'  => view('list.twig', ['list' => $list], false),
+        ];
+
+        return view('home.twig', $data);
+    }
+
+    #[Route(filter: __NAMESPACE__ . '\is_set')]
     public function show($id)
     {
-        return new HtmlResponse('Article ' . $id);
+        $article = $this->repo->getArticle($id);
+
+        $data = [
+            'title' => 'Article ' . $id,
+            'menu'  => $this->menu,
+            'cont'  => view('article.twig', $article, false),
+        ];
+        
+        return view('home.twig', $data);
     }
+}
+
+function is_set($route)
+{
+    $id = $route->getParameters()['id'];
+    $article = (new ArticlesRepo())->getArticle($id);
+
+    return $article ? true : false;
 }
