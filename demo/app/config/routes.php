182ab096ae63be3f5f965f2c8cefa939be4266a7
alx <world.zay@gmail.com>
2025-01-10 12:04:34 +0300

    Created FileResponse class

--- demo/app/config/routes.php
+++ demo/app/config/routes.php
@@ -2,11 +2,23 @@
 
 use App\Controller\SimpleController;
 use App\Controller\SimpleHandler;
+use App\FileResponse;
+use Az\Route\Route;
 use HttpSoft\Response\HtmlResponse;
 
+function isFile($route)
+{
+    $file = '../' . $route->getParameters()['file'];
+    return is_file($file);
+}
+
 return [
-    'home'      => ['/', fn() => view('home.twig')],
-    'articles'  => ['/articles', SimpleHandler::class],
-    'article'   => ['/article/{id}', [SimpleHandler::class, 'show'], ['id' => '\d+']],
+    'home'      => ['/', SimpleHandler::class],
+    'articles'  => ['/articles', [SimpleHandler::class, 'list']],
+    'article'   => ['/article/{id}/{slug?}', [SimpleHandler::class, 'show'], ['slug' => '[\w-]*']],
+    'save'      => ['/article/save', [SimpleHandler::class, 'save']],
     'about'     => ['/about/{action?}', SimpleController::class],
+    'file'      => ['/file/{file}', 
+                    #[Route(filter: 'isFile')] fn($file) => new FileResponse($file), 
+                    ['file' => '.*']],
 ];
