b4d0e1324e4cda2ced4b7edab0b31f4e1f68edf1
alx <world.zay@gmail.com>
2025-01-11 11:16:27 +0300

    Updated demo config files

--- demo/app/config/container.php
+++ demo/app/config/container.php
@@ -1,9 +1,8 @@
 <?php
 
-use Az\Route\Matcher;
-use Az\Route\RouteFactory;
 use Az\Route\Router;
 use Az\Route\RouterInterface;
+
 use HttpSoft\Emitter\EmitterInterface;
 use HttpSoft\Emitter\SapiEmitter;
 use HttpSoft\Runner\MiddlewarePipeline;
@@ -17,11 +16,12 @@ use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 return [
+    RouterInterface::class => fn() => new Router('../app/config/routes.php'),
+
     ServerRequestInterface::class => fn() => (new ServerRequestCreator())->create(),
     MiddlewarePipelineInterface::class => fn() => new MiddlewarePipeline(),
     MiddlewareResolverInterface::class => fn(ContainerInterface $c) => new MiddlewareResolver($c),
     EmitterInterface::class => fn() => new SapiEmitter(),
-    RouterInterface::class => fn() => new Router(),
     Environment::class => function () {
         $loader = new FilesystemLoader('../app/views');
         return new Environment($loader);
--- demo/app/config/pipeline.php
+++ demo/app/config/pipeline.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Middleware\RouteBootstrap;
 use App\Middleware\TemplateSettings;
 use Az\Route\Middleware\RouteDispatch;
 use Az\Route\Middleware\RouteMatch;
 
 $this->pipe(TemplateSettings::class);
-$this->pipe(RouteBootstrap::class);
+
 $this->pipe(RouteMatch::class);
 $this->pipe(RouteDispatch::class);
